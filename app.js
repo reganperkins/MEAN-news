d7b14e0da172aa7b84d3f98a02384d597adbeeb1
Regan <reganlperkins@gmail.com>
2015-02-01 19:25:38 -0800

    add comment ability, and comment view

--- app.js
+++ app.js
@@ -9,26 +9,31 @@ angular.module('meanNews', ['ui.router'])
         templateUrl: '/home.html',
         controller: 'MainCtrl'
       })
+      .state('posts', {
+        url:'/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostCtrl'
+      })
       $urlRouterProvider.otherwise('home');
     }
   ])
-
+  
   .factory('posts', [function(){
     var o = {
       posts: [
-        {title: 'post 1', upvotes: 5 },
-        {title: 'post 2', upvotes: 2 },
-        {title: 'post 3', upvotes: 15 },
-        {title: 'post 4', upvotes: 10 }
+        // {title: 'post 1', upvotes: 5 },
+        // {title: 'post 2', upvotes: 2 },
+        // {title: 'post 3', upvotes: 15 },
+        // {title: 'post 4', upvotes: 10 }
       ]
     };
     return o;
   }])
 
   .controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
+    '$scope',
+    'posts',
+    function($scope, posts){
       $scope.posts = posts.posts;
 
       $scope.addPost = function(){
@@ -36,13 +41,36 @@ angular.module('meanNews', ['ui.router'])
         $scope.posts.push({
           title: $scope.title,
           link: $scope.link, 
-          upvote: 0 
+          upvote: 0,
+          comments: [
+            {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+          ]
         });
         $scope.title = '';
         $scope.link = '';
       };
       $scope.incrementUpvotes = function(post){
         post.upvotes += 1;
+      };
+    }
+  ])
+
+  .controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+      $scope.post = posts.posts[$stateParams.id];
+
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
       };
     }
   ]);
