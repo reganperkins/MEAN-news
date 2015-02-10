cbb69b479e9ebab393d016e6e9342eb2acb74e5c
Regan <reganlperkins@gmail.com>
2015-02-09 20:48:29 -0800

    addd first posts routes and debug schema

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
 mongoose.connect('mongodb://localhost/news');
-require('models.Posts');
-require('modals.Comments');
+
+var routes = require('./routes/index'); //this needs to be called after mongoose schemas are set (as above)
+var users = require('./routes/users');
 
 var app = express();
 
